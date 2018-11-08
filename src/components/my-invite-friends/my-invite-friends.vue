6433be85f12d0da6e00756501b58e7eb2c210c08
kangyilong <1603049109@qq.com>
2018-11-08 21:09:16 +0800

    k

--- src/components/my-invite-friends/my-invite-friends.vue
+++ src/components/my-invite-friends/my-invite-friends.vue
@@ -3,13 +3,31 @@
       <div class='friends icon'>
           <div class='content'>
             <div class="pic icon"></div>
-            <div id='qrcode'></div>
+            <div class="yq-box">
+                <p class="yq_p1">{{nickName}}</p>
+                <p class="yq_p2">{{$t('myInviteFriends.subject.yqnjr')}}</p>
+            </div>
+            <div class="rq-code">
+                <div id='qrcode'></div>
+            </div>
+            <div class="qr-txt">
+                {{$t('myInviteFriends.subject.fuzzs')}}
+                <span @click="isFz = false;">{{$t('myInviteFriends.subject.fz')}}</span>
+            </div>
           </div>
       </div>
       <div class='main-btn'>
           <span class="icon ic_bz"></span>
-          保存至本地
+          {{$t('myInviteFriends.subject.bzbd')}}
       </div>
+      <div class="ress-box" :class="{'hidden': isFz}">
+          <textarea class="ress" type="text" id="copyObj" readonly v-model="wxUrl">
+          </textarea>
+          <div class="ress-btn" @click='CopyUrl'>
+              复制
+          </div>
+      </div>
+    <Toast :text="textMsg" ref="toast" />
     <FullLoading ref="fullLoading" v-show="isLoading"/> 
   </div>
 </template> 
@@ -17,6 +35,7 @@
 const QRCode = require('js-qrcode');
 import {getUserId, setTitle} from 'common/js/util';
 import FullLoading from 'base/full-loading/full-loading';
+import Toast from 'base/toast/toast';
 import {getUser} from 'api/user';
 
 export default {
@@ -24,7 +43,11 @@ export default {
   data() {
     return {
         nickName: '',
-        isLoading: true
+        isLoading: true,
+        wxUrl: '',
+        userId: '',
+        isFz: true,
+        textMsg: ''
     };
   },
   mounted() {
@@ -45,8 +68,25 @@ export default {
     });
     qr.make(this.wxUrl);
   },
+  methods: {
+    CopyUrl() {
+        let url = document.querySelector('#copyObj');
+        url.select(); // 选择对象
+        if(!document.execCommand('Copy')){
+            this.textMsg = this.$t('walletInto.subject.gbzc');
+            this.$refs.toast.show();
+        }else{
+            this.textMsg = '复制成功';
+            this.$refs.toast.show();
+            setTimeout(() => {
+                this.isFz = true;
+            }, 1000);
+        }
+    }
+  },
   components: {
-      FullLoading
+      FullLoading,
+      Toast
   }
 };
 </script>
@@ -82,19 +122,55 @@ export default {
         .content {
             width: 100%;
             text-align: center;
-            position: absolute;
-            top: .96rem;
+            overflow: hidden;
             .logo {
                 width: 1.1rem;
                 height: .48rem;
-                padding-bottom: .74rem;
             }
             .pic {
-                width: 3.89rem;
+                width: 2.5rem;
                 margin: 0 auto;
-                height: .63rem;
-                @include bg-image("FUNMVP");
-                margin-bottom: .4rem;
+                height: 2.25rem;
+                background-image: url('./logo.png');
+                margin-top: 1.06rem;
+                margin-bottom: 1rem;
+            }
+            .yq-box{
+                color: #fff;
+                margin-bottom: 0.32rem;
+                p{
+                    margin-top: 0.22rem;
+                }
+                .yq_p1{
+                    font-size: 0.32rem;
+                }
+                .yq_p2{
+                    font-size: 0.28rem;
+                }
+            }
+            .rq-code{
+                margin: 0 auto;
+                width: 3.6rem;
+                height: 3.6rem;
+                border-radius: 0.12rem;
+                margin-bottom: 0.38rem;
+                background-color: #fff;
+                padding: 0.28rem;
+            }
+            .qr-txt{
+                height: 0.96rem;
+                line-height: 0.96rem;
+                color: #fff;
+                font-size: 0.26rem;
+                span{
+                    display: inline-block;
+                    border-radius: 0.04rem;
+                    border: 1px solid #fff;
+                    margin-left: 0.2rem;
+                    line-height: 1;
+                    vertical-align: middle;
+                    padding: 0.1rem 0.25rem;
+                }
             }
         }
     }
@@ -104,9 +180,12 @@ export default {
         width: 100%;
         height: 0.98rem;
         line-height: 0.98rem;
-        font-size: 0.6rem;
+        font-size: 0.3rem;
         color: #fff;
         text-align: center;
+        background-color: #141e68;
+        opacity: 0.8;
+        border-bottom: 2px solid #687cfd;
         .ic_bz{
             display: inline-block;
             width: 0.3rem;
@@ -114,6 +193,41 @@ export default {
             background-image: url('./bcbd.png');
             margin-right: 0.14rem;
             vertical-align: middle;
+        }
+    }
+    .ress-box{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 80%;
+        height: 5rem;
+        border-radius: 0.2rem;
+        padding: 0.3rem;
+        background-color: #fff;
+        .ress{
+            width: 100%;
+            height: 3rem;
+            background-color: #fff;
+            border: 1px solid #ddd;
+            padding: 0.15rem;
+            padding-top: 0.2rem;
+            border-radius: 0.1rem;
+            font-size: 0.34rem;
+            line-height: 1.4;
+            word-wrap: break-word;
+        }
+        .ress-btn{
+            margin-top: 0.35rem;
+            width: 100%;
+            height: 1rem;
+            line-height: 1rem;
+            text-align: center;
+            border: 1px solid #ddd;
+            font-size: 0.34rem;
+            letter-spacing: 0.04rem;
+            color: #333;
+            border-radius: 0.06rem;
         }
     }
 
--- src/components/shop-cz/shop-cz.vue
+++ src/components/shop-cz/shop-cz.vue
@@ -1,28 +1,79 @@
 <template>
     <div>
         <div class="cz-box">
-            <p>充值数量：</p>
-            <div class="cz-iup"><input type="text" v-model="czNumber" placeholder="请输入充值数量"></div>
+            <p>充值FMVP</p>
+            <div class="cz-iup"><input type="number" v-model="count" placeholder="请输入充值数量"></div>
+            <div class="cz-iup"><input type="password" v-model="config.tradePwd" placeholder="请输入交易密码"></div>
         </div>
         <div class="qr-box" @click="qrczFn">
             确定
         </div>
+      <Toast :text="textMsg" ref="toast" />
+      <FullLoading ref="fullLoading" v-show="isLoading"/>
     </div>
 </template>
 
 <script>
-import { setTitle } from "common/js/util";
+import { rechargeGram } from 'api/store';
+import { getUser } from 'api/person';
+import { setTitle, getUserId, formatMoneyMultiply } from "common/js/util";
+import FullLoading from 'base/full-loading/full-loading';
+import Toast from 'base/toast/toast';
     export default {
         data(){
             return {
-                czNumber: 0
+                config: {
+                    userId: getUserId(),
+                    count: '',
+                    tradePwd: ''
+                },
+                count: '',
+                textMsg: '',
+                isLoading: false
             }
         },
         created() {
             setTitle('充值');
         },
         methods: {
-            qrczFn(){}
+            qrczFn(){
+                if(this.count == ''){
+                    this.textMsg = '充值数量不能为空';
+                    this.$refs.toast.show();
+                    return;
+                }else if(this.config.tradePwd == ''){
+                    this.textMsg = '请输入交易密码';
+                    this.$refs.toast.show();
+                    return;
+                }
+                getUser(getUserId()).then(res => {
+                    if (res.tradepwdFlag) {
+                        this.config.count = formatMoneyMultiply(this.count, '', 'FMVP');
+                        rechargeGram(this.config).then(data => {
+                            if(data.code){
+                                this.textMsg = '充值成功';
+                                this.$refs.toast.show();
+                                setTimeout(() => {
+                                    this.$router.push('/shop');
+                                }, 1500);
+                            }
+                        }, () => {
+                            this.count = '';
+                            this.config.tradePwd = '';
+                        });
+                    } else{
+                        this.textMsg = '请先设置资金密码';
+                        this.$refs.toast.show();
+                        setTimeout(function () {
+                            this.$router.push('security-tradePassword?istw=0');
+                        }, 1800)
+                    } 
+                });
+            }
+        },
+        components: {
+            Toast,
+            FullLoading
         }
     }
 </script>
@@ -37,12 +88,15 @@ import { setTitle } from "common/js/util";
 .cz-box p{
     margin-bottom: 0.4rem;
 }
+.cz-iup{
+    margin-top: 0.3rem;
+}
 .cz-iup input{
     width: 100%;
     padding: 0.2rem 0;
     font-size: 0.32rem;
     color: #333;
-    border-bottom: 1px solid #ccc;
+    border-bottom: 1px solid #ddd;
 }
 .qr-box{
     margin: 0 auto;
